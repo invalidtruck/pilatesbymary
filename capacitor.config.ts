ab22d5c405c1c8fb531fbb4aeb41e4aae279bd5c
David Silva <invalidtruck@msn.com>
2023-10-06 17:07:16 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pilatesbymary',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
